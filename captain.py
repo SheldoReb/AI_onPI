d3c5d917e21d2d012185ee0194914a08b90af7d4
Rebernik <sheldon.rebernik@munichmotorsport.de>
2025-01-03 21:06:13 +0100

    Add .gitignore and enhance captain.py with local executor support

--- captain.py
+++ captain.py
@@ -3,6 +3,9 @@ from autogen import UserProxyAgent
 from autogen.agentchat.contrib.captainagent import CaptainAgent
 from autogen.coding import DockerCommandLineCodeExecutor
 import tempfile
+from pathlib import Path
+from autogen.coding import CodeBlock, LocalCommandLineCodeExecutor
+
 
 # Create a temporary directory for code files
 temp_dir = tempfile.TemporaryDirectory()
@@ -15,27 +18,35 @@ config_list = autogen.config_list_from_json(
 llm_config = {"temperature": 0, "config_list": config_list}
 
 # Set up the Docker executor
-executor = DockerCommandLineCodeExecutor(
+docker_executor = DockerCommandLineCodeExecutor(
     image="python:3.12-slim",
     timeout=3600,  # Set a long timeout (e.g., 1 hour) to keep the container alive
     work_dir=temp_dir.name,
     stop_container=False  # This flag keeps the container running
 )
 
+# Set up the Local executor
+work_dir = Path("executor_work_dir")
+work_dir.mkdir(exist_ok=True)
+
+local_executor = LocalCommandLineCodeExecutor(work_dir=work_dir)
+
 # build agents
 captain_agent = CaptainAgent(
     name="captain_agent",
     llm_config=llm_config,
-    code_execution_config={"use_docker": executor},
-    agent_config_save_path=None,  # If you'd like to save the created agents in nested chat for further use, specify the save directory here
+    code_execution_config={"executer": local_executor},
+    agent_config_save_path="captain_agent_config.json",  
 )
+
+
 captain_user_proxy = UserProxyAgent(name="captain_user_proxy", human_input_mode="ALWAYS")
 
 USER_QUERY = """
-setup home assistant with docker and check if you can use the home assistant api for configuration and management.
+setup docker and home assistant with docker and check if you can use the home assistant api for configuration and management.
 """
 
-QUERY = f"""YOUR ROLE: You are managing a Raspberry 3 B with docker installed. 
+QUERY = f"""YOUR ROLE: You are managing a Raspberry 3 B. 
 All USER_QUERIES have to be done on the Raspberry 3 B.
 Your main mission is to manage a smart home setup with home assistant.
 
@@ -48,8 +59,15 @@ result = captain_user_proxy.initiate_chat(
     captain_agent,
     message=QUERY,
     max_turns=10,
+    clear_history=False
 )
 
+print("Zwischenstop")
 
+result = captain_user_proxy.initiate_chat(
+    captain_agent,
+    message="What was our last conversation about?",
+    max_turns=2,
+    clear_history=False
+)
 print("Done")
-executor.stop()
