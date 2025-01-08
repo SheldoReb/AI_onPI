34ed366473fd36ab0f4a6ab51f2adb0e00af40f5
Rebernik <sheldon.rebernik@munichmotorsport.de>
2025-01-08 22:14:51 +0100

    zwischenstand

--- captain.py
+++ captain.py
@@ -8,7 +8,7 @@ from autogen.coding import DockerCommandLineCodeExecutor
 
 
 # Create a temporary directory for code files
-temp_dir = tempfile.TemporaryDirectory()
+temp_dir = tempfile.TemporaryDirectory(dir="groupchat")
 
 config_path = "OAI_CONFIG_LIST"
 config_list = autogen.config_list_from_json(
@@ -19,8 +19,7 @@ llm_config = {"temperature": 0, "config_list": config_list}
 
 
 # Set up the Local executor
-work_dir = Path("groupchat")
-work_dir.mkdir(exist_ok=True)
+work_dir = temp_dir.name 
 
 local_executor = LocalCommandLineCodeExecutor(work_dir=work_dir)
 
@@ -32,40 +31,69 @@ use_docker = False
 DESCRIPTION = f"""YOUR ROLE: You are managing a Raspberry 3 B, by building a group of agents at a proper time to solve a task. 
 All USER_QUERIES have to be done on the Raspberry 3 B.
 Your main mission is to manage a smart home setup with home assistant.
+Home Assistant is setup up and running on the Raspberry with home assistant OS.
 """.strip()
+
+NESTED_CONFIG = {
+    "autobuild_init_config": {
+        "config_file_or_env": "OAI_CONFIG_LIST",
+        "builder_model": "gpt-4o",
+        "agent_model": "gpt-4o",
+    },
+    "autobuild_build_config": {
+        "default_llm_config": {"temperature": 1, "top_p": 0.95, "max_tokens": 2048},
+        "code_execution_config": {
+            "timeout": 300,
+            "work_dir": str(work_dir),
+            "last_n_messages": 1,
+            "use_docker": False,
+        },
+        "coding": True,
+    },
+    "group_chat_config": {"max_round": 10},
+    "group_chat_llm_config": None,
+    "max_turns": 10,
+}
 
 
 captain_agent = CaptainAgent(
     name="captain_agent",
 #    description=DESCRIPTION,
     llm_config=llm_config,
-#    nested_config=DEFAULT_NESTED_CONFIG,
-    code_execution_config={
-                           "executer": executer,
-                           "use_docker": use_docker,
-                           "last_n_messages": 1},
-#    agent_config_save_path="captain_agent_configs",
+    nested_config=NESTED_CONFIG,
+    agent_config_save_path="captain_agent_configs",
     agent_lib="expert_library/expert_library.json", 
     tool_lib="tools" 
 )
 
 captain_user_proxy = UserProxyAgent(name="captain_user_proxy", 
-                                    human_input_mode="ALWAYS",
-                                    code_execution_config={"executer": executer,
-                                                           "use_docker": use_docker})
+                                    human_input_mode="ALWAYS")
+
+HA_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwNTlhZWEyYmYwNDg0ZGZjODQxMTlhMWE2OTg5YjkwOSIsImlhdCI6MTczNjM3MDc1MSwiZXhwIjoyMDUxNzMwNzUxfQ.zj3TL1qGdf7nlYlFaz66H-aLTWsFCS7UMliRlogtGF4"
+SHELLYBLU_GATEWAY1="192.168.178.194"
 
 USER_QUERY = """
-Get all infos from https://developers.home-assistant.io/docs/api/rest/ use website scraping.
-"""
-#Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiMGE0ODNlZjdmZDg0MTE4YTM0ZjJhMDk0OGUxY2QyYiIsImlhdCI6MTczNjA5NTQ3NCwiZXhwIjoyMDUxNDU1NDc0fQ.CPNf9678eeEVcxf01aFjeDr2DyLm3bWgOjDOvQtrAHY
-#to check if the token is still valid.
-#Then get all configs needed to configure the home assistant.
-#""".strip()
+List of local devices, environmental variables are loaded in your environment.
+Name                    docs_url                                                                environmental variables
+IP_ADRESS_HOMEASSITANT  https://developers.home-assistant.io/docs/api/rest/                     IP_ADRESS_HOMEASSITANT
+ShellyBlueGateway1                                                                              SHELLYBLU_GATEWAY1
+ShellyScriptsTutorial   https://shelly-api-docs.shelly.cloud/gen2/Scripts/Tutorial                                      
+
+Set up MQTT docker container on Raspberry 3 B, docker already installed.
+Set up MQTT in homa assistant over tha api.
+Set up MQTT for ShellyBlueGateway1, so that we can control it over home assistant. 
+
+REMEMBER EVERYBPODY TO USE TOOLS PROVIDED 
+""".strip()
 
 result = captain_user_proxy.initiate_chat(
     captain_agent,
     message=USER_QUERY,
-    max_turns=10
+    max_turns=10,
+    clear_history=False,
 )
 
+# Clean up the temporary directory after user input confirmation
+input("Press Enter to confirm cleanup...")
+temp_dir.cleanup()
 print("Done")
